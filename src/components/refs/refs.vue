81becc876e49ca45c1e0aa31e9044ff97031de0a
JaehonDev <println@kakao.com>
2022-06-15 10:02:39 +0900

    refs

---
+++ src/components/refs/refs.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        1,
+          2
+      ]
+    }
+  },
+  mounted() {
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list" ref="items">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
