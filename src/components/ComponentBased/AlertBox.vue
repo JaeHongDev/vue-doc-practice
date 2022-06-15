2194756bcdee02c6f7d55370a237eea87b90843b
JaehonDev <println@kakao.com>
2022-06-15 10:38:44 +0900

    component based

---
+++ src/components/ComponentBased/AlertBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="alert-box">
+    <strong>
+      This is an Error for Demo Purpose
+    </strong>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlertBox"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ComponentBased/ComponentBased.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <h1>button counter</h1>
+  <button-counter></button-counter>
+
+
+  <div :style="{ fontSize: postFontSize+'em'}">
+    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" @enlarge-text="postFontSize += 0.1"/>
+  </div>
+
+  <alert-box>
+    <div :style="{ fontSize: postFontSize+'em'}">
+      <BlogPost v-for="post in posts" :key="post.id" :title="post.title" @enlarge-text="postFontSize += 0.1"/>
+    </div>
+  </alert-box>
+</template>
+<script>
+import ButtonCounter from "./ButtonCounter.vue";
+import BlogPost from "./BlogPost.vue";
+import AlertBox from "./AlertBox.vue";
+export default {
+  components: {AlertBox, BlogPost, ButtonCounter},
+  data() {
+    return {
+      count: 0,
+      posts: [
+        { id: 1, title: 'My journey with Vue' },
+        { id: 2, title: 'Blogging with Vue' },
+        { id: 3, title: 'Why Vue is so fun' }
+      ],
+      postFontSize:1
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
